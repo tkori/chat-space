10a46af79cf9b21f5a1d5b03bdb7d61fd782cee5
tkori <tkori19+github@gmail.com>
2019-07-18 19:59:35 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,15 +3,16 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
+  config.storage = :fog
+  config.fog_provider = 'fog/aws'
+  config.fog_directory = 'bucket-tkori-chat-space'
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
 
-    config.storage = :fog
-    config.fog_provider = 'fog/aws'
-    config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      region: 'ap-northeast-1'
-    }
+    region: 'ap-northeast-1'
+  }
 
   config.fog_directory  = 'bucket-tkori-chat-space'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/bucket-tkori-chat-space'
