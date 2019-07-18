bdd10fab06b91ffbd3df4b8ff29fb1b20ee26a2a
tkori <tkori19+github@gmail.com>
2019-07-18 14:54:33 +0900

    Configure new IAM info

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,6 +12,6 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1'
   }
 
-  config.fog_directory  = 'tkori-chat-space'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/tkori-chat-space'
+  config.fog_directory  = 'tkori-chat-space-bucket'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/tkori-chat-space-bucket'
 end
